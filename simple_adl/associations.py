eb4e3e4c327ecd2c6e54ebee9b64ee4a1860ed9c
William Cerny <Willcerny@gmail.com>
2020-12-14 01:29:56 -0600

    Update associations.py

--- simple_adl/associations.py
+++ simple_adl/associations.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python
+"""
+Generic python script.
+"""
+__author__ = "William Cerny and Sidney Mau"
 
+import numpy as np
+import astropy
+from astropy.coordinates import SkyCoord
+import astropy.units as u
+#-------------------------------------------------------------------------------
